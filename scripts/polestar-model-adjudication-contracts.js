a3609dd9b0f53e190a0c5e0f0b24761a63788f20
dvon24 <devonsroberson24@yahoo.com>
2026-08-10 15:13:13 +0200

    Audit Polestar 2 known issues

--- scripts/polestar-model-adjudication-contracts.js
+++ scripts/polestar-model-adjudication-contracts.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const polestar1Contract = require('./polestar-1-adjudication-contract');
-const CONTRACTS = Object.freeze({ 'Polestar 1': polestar1Contract });
+const polestar2Contract = require('./polestar-2-adjudication-contract');
+const CONTRACTS = Object.freeze({ 'Polestar 1': polestar1Contract, 'Polestar 2': polestar2Contract });
 function getContract(model) { const contract = CONTRACTS[model]; if (!contract) throw new Error(`Unsupported Polestar model: ${model}`); return contract; }
 module.exports = { CONTRACTS, getContract };
