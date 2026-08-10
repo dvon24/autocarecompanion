a8a827ba22b8c30c5993e4adfdc9dce2f90764d3
dvon24 <devonsroberson24@yahoo.com>
2026-08-10 16:32:22 +0200

    Audit Pontiac Bonneville known issues

--- scripts/pontiac-model-adjudication-contracts.js
+++ scripts/pontiac-model-adjudication-contracts.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const aztek = require('./pontiac-aztek-adjudication-contract');
-const CONTRACTS = Object.freeze({ Aztek: aztek });
+const bonneville = require('./pontiac-bonneville-adjudication-contract');
+const CONTRACTS = Object.freeze({ Aztek: aztek, Bonneville: bonneville });
 function getContract(model) { const contract = CONTRACTS[model]; if (!contract) throw new Error(`Unknown Pontiac model: ${model}`); return contract; }
 module.exports = { CONTRACTS, getContract };
