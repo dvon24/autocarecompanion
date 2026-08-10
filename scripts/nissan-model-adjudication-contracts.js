6cf16d9e2ca4db0eb1c4d458345c6a7bd540faba
dvon24 <devonsroberson24@yahoo.com>
2026-08-10 04:45:34 +0200

    Audit Nissan 300ZX known issues

--- scripts/nissan-model-adjudication-contracts.js
+++ scripts/nissan-model-adjudication-contracts.js
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const twoFortySxContract = require('./nissan-240sx-adjudication-contract');
+const threeHundredZxContract = require('./nissan-300zx-adjudication-contract');
 
 const CONTRACTS = Object.freeze({
   '240SX': twoFortySxContract,
+  '300ZX': threeHundredZxContract,
 });
 
 function getContract(model) {
