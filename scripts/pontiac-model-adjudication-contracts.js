42ddff1a53d6468ca3c10caad07f3ef031f5fba1
dvon24 <devonsroberson24@yahoo.com>
2026-08-10 16:39:07 +0200

    Audit Pontiac Firebird known issues

--- scripts/pontiac-model-adjudication-contracts.js
+++ scripts/pontiac-model-adjudication-contracts.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const aztek = require('./pontiac-aztek-adjudication-contract');
 const bonneville = require('./pontiac-bonneville-adjudication-contract');
-const CONTRACTS = Object.freeze({ Aztek: aztek, Bonneville: bonneville });
+const firebird = require('./pontiac-firebird-adjudication-contract');
+const CONTRACTS = Object.freeze({ Aztek: aztek, Bonneville: bonneville, Firebird: firebird });
 function getContract(model) { const contract = CONTRACTS[model]; if (!contract) throw new Error(`Unknown Pontiac model: ${model}`); return contract; }
 module.exports = { CONTRACTS, getContract };
