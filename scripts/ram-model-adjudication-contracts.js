1d69ab788a2cb094a1fb9bed5b4f7e1fe270e94e
dvon24 <devonsroberson24@yahoo.com>
2026-08-11 00:55:08 +0200

    Audit RAM ProMaster known issues

--- scripts/ram-model-adjudication-contracts.js
+++ scripts/ram-model-adjudication-contracts.js
@@ -3,6 +3,7 @@ const contracts = Object.freeze({
   '1500 Classic': require('./ram-1500-classic-adjudication-contract'),
   '2500': require('./ram-2500-adjudication-contract'),
   '3500': require('./ram-3500-adjudication-contract'),
+  ProMaster: require('./ram-promaster-adjudication-contract'),
 });
 
 function getContract(model) {
