7e2bbce40ea2c528e9b92866611ac187d79349d4
dvon24 <devonsroberson24@yahoo.com>
2026-08-10 13:47:47 +0200

    Audit Peugeot 308 known issues

--- scripts/peugeot-model-adjudication-contracts.js
+++ scripts/peugeot-model-adjudication-contracts.js
@@ -2,6 +2,7 @@
 const peugeot206Contract = require('./peugeot-206-adjudication-contract');
 const peugeot207Contract = require('./peugeot-207-adjudication-contract');
 const peugeot208Contract = require('./peugeot-208-adjudication-contract');
-const CONTRACTS = Object.freeze({ '206': peugeot206Contract, '207': peugeot207Contract, '208': peugeot208Contract });
+const peugeot308Contract = require('./peugeot-308-adjudication-contract');
+const CONTRACTS = Object.freeze({ '206': peugeot206Contract, '207': peugeot207Contract, '208': peugeot208Contract, '308': peugeot308Contract });
 function getContract(model) { const contract = CONTRACTS[model]; if (!contract) throw new Error(`Unsupported Peugeot model: ${model}`); return contract; }
 module.exports = { CONTRACTS, getContract };
