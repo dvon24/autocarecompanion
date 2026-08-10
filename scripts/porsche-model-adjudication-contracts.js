ffa060efe06de25c8186d015ff51d41700641784
dvon24 <devonsroberson24@yahoo.com>
2026-08-10 21:22:34 +0200

    Audit Porsche 911 known issues

--- scripts/porsche-model-adjudication-contracts.js
+++ scripts/porsche-model-adjudication-contracts.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const boxster718 = require('./porsche-718-boxster-adjudication-contract');
 const cayman718 = require('./porsche-718-cayman-adjudication-contract');
-const CONTRACTS = Object.freeze({ '718 Boxster': boxster718, '718 Cayman': cayman718 });
+const porsche911 = require('./porsche-911-adjudication-contract');
+const CONTRACTS = Object.freeze({ '718 Boxster': boxster718, '718 Cayman': cayman718, '911': porsche911 });
 function getContract(model) { const contract = CONTRACTS[model]; if (!contract) throw new Error(`Unknown Porsche model: ${model}`); return contract; }
 module.exports = { CONTRACTS, getContract };
